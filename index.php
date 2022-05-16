c9b18b6aba497f7c49f5629d6db25804f19acfe7
erikmachadopro <erikmachadopro@gmail.com>
2022-05-16 19:04:20 -0300

    criação da recuperação de senha

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require_once("conexao.php");
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -9,6 +13,10 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
 	<link rel="stylesheet" href="css/login.css">
+
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 	<!--REFERENCIA PARA O FAVICON -->
 	<link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
@@ -38,9 +46,64 @@
 					<input type="checkbox">
 					Lembrar-me
 				</label>
-				<a href="#" class="float-right">Recuperar Senha</a>
+				<a class="float-right" data-toggle="modal" data-target="#modal-senha">Recuperar Senha</a>
 			</div>
 		</form>
 	</div>
 </body>
 </html>
+
+<!-- MODAL RECUPERAR SENHA -->
+<div class="modal fade" id="modal-senha" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title text-dark">Recuperar Senha</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+	  <form method="post">
+		<div class="modal-body">
+			<div class="form-group">
+				<label class="text-dark" for="emailRecuperar">Insira seu e-mail para recuperar a senha</label>
+				<input type="email" class="form-control" name="txtEmail" id="emailRecuperar" aria-describedby="emailHelp" placeholder="nome@email.com">	
+			</div>
+		</div>
+		<div class="modal-footer">
+			<button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
+			<button type="submit" name="recuperar-senha" class="btn btn-success">Recuperar</button>
+		</div>
+	  </form>
+    </div>
+  </div>
+</div>
+
+
+<!-- CÓDIGO DO BOTÃO RECUPERAR -->
+<?php
+    if(isset($_POST['recuperar-senha'])){
+		$email_usuario = $_POST['txtEmail'];
+
+		/* variável $res retorna o resultado da consulta feita no BD */
+		$res = $pdo->prepare(" SELECT * from usuarios where usuario = :usuario");
+
+		$res->bindValue(":usuario", $email_usuario);
+		$res-> execute();
+	
+		/* variável $dados é o retorno de todas informações obtidas no BD */
+		$dados = $res->fetchAll(PDO::FETCH_ASSOC);
+		$linhas = count($dados);
+	
+		if($linhas > 0){
+			$nome_usu = $dados[0]['nome'];
+			$senha_usu = $dados[0]['senha_original'];
+			$nivel_usu = $dados[0]['nivel'];
+		} else{
+			echo "<script language='javascript'>
+					window.alert('E-mail não cadastrado.');
+					</script>";
+		}
+	}
+?>
+

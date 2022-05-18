0a0bae9eaeafcf8c4288c4efd1e0e7ac1190f735
erikmachadopro <erikmachadopro@gmail.com>
2022-05-18 16:20:28 -0300

    envio de arquivo do BD

--- config.php
+++ config.php
@@ -12,7 +12,7 @@ $opcao2 = 4;
 $opcao3 = 6;
 
 // VARIAVEL PARA DEFINIR O CAMINHO DO SISTEMA
-$url_site = 'http://localhost/estudos/php7-sistema-hospitalar/sysmedical/';
-$email_adm = 'erikmachadopro@outlook.com';
+$url_site = 'http://localhost/sysmedical/';
+$email_adm = 'seu-email@email.com';
 
 ?>

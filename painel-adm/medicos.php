a290ab8ed1914b37069ff4a7b4f5799d05bd7af6
erikmachadopro <erikmachadopro@gmail.com>
2022-05-21 16:24:24 -0300

    cadastro de médico via script ajax

--- painel-adm/medicos.php
+++ painel-adm/medicos.php
@@ -1,3 +1,5 @@
+<?php $pagina = 'medicos'; ?>
+
 <div class="row">
     <div class="col-md-12 botao-novo">
         <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modal">Novo Médico</button>
@@ -18,8 +20,8 @@
     <div class="col-md-6 col-sm-12">
         <div class="float-right mr-4">
             <form class="form-inline my-2 my-lg-0">
-                <input class="form-control-sm mr-sm-2" type="search" placeholder="Nome ou CRM" aria-label="Pesquisar" name="txtbuscar">
-                <button class="btn btn-outline-secondary btn-sm my-2 my-sm-0" type="submit" name="<?php echo $item2; ?>"><i class="fas fa-search"></i></button>
+                <input class="form-control-sm mr-sm-2" type="search" placeholder="Nome ou CRM" aria-label="Pesquisar" name="txtbuscar" id="txtbuscar">
+                <button class="btn btn-outline-secondary btn-sm my-2 my-sm-0" name="btn-buscar" id="btn-buscar"><i class="fas fa-search"></i></button>
             </form>
         </div>  
     </div>
@@ -27,100 +29,124 @@
 
 <div class="table-responsive-sm mt-3">
     <table class="table table-sm">
-    <thead class="thead-light">
-        <tr>
-        <th scope="col">Nome</th>
-        <th scope="col">Especialidade</th>
-        <th scope="col">CRM</th>
-        <th scope="col">Telefone</th>
-        <th scope="col">Ações</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Mark</td>
-            <td>Clínico Geral</td>
-            <td>GO/123456</td>
-            <td>(62) 99999-9999</td>
-            <td><a href="#"><i class="fas fa-edit text-info"></i></a>
-                <a href="#"><i class="far fa-trash-alt text-danger"></i></a>
-            </td>
-        </tr>
-    </tbody>
+        <thead class="thead-light">
+            <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Especialidade</th>
+            <th scope="col">CRM</th>
+            <th scope="col">Telefone</th>
+            <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Mark</td>
+                <td>Clínico Geral</td>
+                <td>GO/123456</td>
+                <td>(62) 99999-9999</td>
+                <td><a href="#"><i class="fas fa-edit text-info"></i></a>
+                    <a href="#"><i class="far fa-trash-alt text-danger"></i></a>
+                </td>
+            </tr>
+        </tbody>
     </table>
 </div>
 
 <!-- MODAL PARA CADASTRO --> 
-<!-- Modal -->
+
 <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Cadastro de Médicos</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-
-        <form>
-            <div class="row">
-                <div class="col-md-6 col-sm-12">
-                    <div class="form-group">
-                        <label for="">Nome</label>
-                        <input type="text" class="form-control" id="" placeholder="Insira o nome" name="nome">
-                    </div>
-                </div>
-                <div class="col-md-6 col-sm-12">
-                    <div class="form-group">
-                        <label for="">Especialidade</label>
-                        <select class="form-control" id="" name="especialidade">
-                            <option>1</option>
-                        </select>
-                    </div>                    
-                </div>
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Cadastro de Médicos</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-
-            <div class="row">
-                <div class="col-md-4 col-sm-12">
-                    <div class="form-group">
-                        <label for="">CRM</label>
-                        <input type="text" class="form-control" id="crm" placeholder="Insira o CRM" name="crm">
+            <div class="modal-body">
+                <form method="post">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+                            <div class="form-group">
+                                <label for="">Nome</label>
+                                <input type="text" class="form-control" id="nome" placeholder="Insira o nome" name="nome">
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-12">
+                            <div class="form-group">
+                                <label for="">Especialidade</label>
+                                <select class="form-control" id="especialidade" name="especialidade">
+                                    <option>1</option>
+                                </select>
+                            </div>                    
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-4 col-sm-12">
-                    <div class="form-group">
-                        <label for="">CPF</label>
-                        <input type="text" class="form-control" id="cpf" placeholder="Insira o CPF" name="cpf">
-                    </div>                    
-                </div>
-                <div class="col-md-4 col-sm-12">
-                    <div class="form-group">
-                        <label for="">Telefone</label>
-                        <input type="text" class="form-control" id="telefone" placeholder="Insira o CRM" name="telefone">
-                    </div>
-                </div>
-            </div>
-            
-            <div class="form-group">
-                <label for="exampleFormControlInput1">E-mail</label>
-                <input type="email" class="form-control" id="" placeholder="nome@exemplo.com" name="email">
-            </div>
-        </form>
-        
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
-        <form method="post">
-            <button type="submit" name="btn-salvar" class="btn btn-success">Salvar</button>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
 
-<!--MASCARAS -->
+                    <div class="row">
+                        <div class="col-md-4 col-sm-12">
+                            <div class="form-group">
+                                <label for="">CRM</label>
+                                <input type="text" class="form-control" id="crm" placeholder="Insira o CRM" name="crm">
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-12">
+                            <div class="form-group">
+                                <label for="">CPF</label>
+                                <input type="text" class="form-control" id="cpf" placeholder="Insira o CPF" name="cpf">
+                            </div>                    
+                        </div>
+                        <div class="col-md-4 col-sm-12">
+                            <div class="form-group">
+                                <label for="">Celular</label>
+                                <input type="text" class="form-control" id="telefone" placeholder="Insira o celular" name="telefone">
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">E-mail</label>
+                        <input type="email" class="form-control" id="email" placeholder="nome@exemplo.com" name="email">
+                    </div>
+
+                    <div id="mensagem" class="col-md-12 text-center mt-3">
+                        
+                    </div>
+            </div> <!-- fim da modal-body -->    
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                        <button name="btn-salvar" id="btn-salvar" class="btn btn-success">Salvar</button>
+                    </div>
+                </form>
+        </div> <!-- fim da modal-content  -->
+    </div> <!-- fim da modal-dialog -->
+</div> <!-- fim da modal  -->
+
+<!-- MASCARAS -->
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
 
 <script src="../js/mascaras.js"></script>
+
+<!-- AJAX PARA INSERÇÃO DOS DADOS -->
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#btn-salvar').click(function(event){
+            event.preventDefault();
+            $.ajax({
+                url: "medicos/inserir.php",
+                method: "post",
+                data: $('form').serialize(),
+                dataType: "text",
+                success: function(mensagem){
+                    $('#mensagem').removeClass()
+                    if (mensagem =='Registro inserido com sucesso.') {
+                        $('#mensagem').addClass('mensagem-sucesso')
+                    }else{
+                        $('#mensagem').addClass('mensagem-erro')
+                    }
+                    $('#mensagem').text(mensagem)
+                },
+            })
+        })
+    })
+</script>

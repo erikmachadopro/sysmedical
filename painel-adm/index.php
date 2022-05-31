f106a2248ebe9d077e239addbd105c815f2696b2
erikmachadopro <erikmachadopro@gmail.com>
2022-05-31 08:11:17 -0300

    correção dos caracteres especiais

--- painel-adm/index.php
+++ painel-adm/index.php
@@ -40,7 +40,7 @@ $item5 = 'notificacoes';
     <head>
         <title>Painel Administrativo</title>
 
-        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
